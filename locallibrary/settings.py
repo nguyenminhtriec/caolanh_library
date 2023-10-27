788c09db5c0136885de0f3ea32637652c4e97929
nguyenminhtriec <60143102+nguyenminhtriec@users.noreply.github.com>
2023-10-27 15:38:51 +0700

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -149,6 +150,16 @@ if 'DATABASE_URL' in os.environ:
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Static file serving.
+# https://whitenoise.readthedocs.io/en/stable/django.html#add-compression-and-caching-support
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
