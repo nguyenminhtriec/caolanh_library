173409105536abcaf164d5993b50d20955831a06
nguyenminhtriec <60143102+nguyenminhtriec@users.noreply.github.com>
2023-10-27 15:07:16 +0700

    Update settings.py: add DATABASE_URL setting

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -135,3 +135,13 @@ LOGIN_REDIRECT_URL = '/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# Update database configuration from $DATABASE_URL environment variable (if defined)
+import dj_database_url
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=500,
+        conn_health_checks=True,
+    )
+
+
